4452c927c7e9746e30fb18fa8a5dcce790bb1b2f
liliani <ligabe16@icloud.com>
2019-02-04 23:10:59 -0800

    added models src and client

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+/////////////////////////////////////////////// /* Components */ //////////////////////////////////////////////////////////
+// import Nav from './components/NavBar';
+import Home from './pages/Home';
+import Saved from './pages/Saved';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+          <Router>
+            <switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/Saved" component={Saved}/>
+            </switch>
+          </Router>
+      </div>
+    );
+  }
+}
+
+export default App
